9b266e487f632b34ea5fc66eff559d348d8addfe
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2024-04-14 23:41:57 +0800

    WTForms study

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired  # 檢查資料是否為空
 
-app = Flask(__name__)  # create an Flask instances
+app = Flask(__name__)  # create a Flask instances
+app.config['SECRET_KEY'] = 'flasker'  # set a secret key for the application
+
+
+# create a form class
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 # create a route decorator
@@ -38,6 +48,18 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+
+
+# create Name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NameForm()
+    # Validate the form
+    if form.validate_on_submit():  # check if the form is submitted
+        name = form.name.data
+        form.name.data = ''
+    return render_template('name.html', form=form, name=name)
 
 
 if __name__ == '__main__':
