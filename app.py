d5bcce2c4ab6376231d4d29c1287e919349147f2
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2024-04-28 23:53:46 +0800

    sqlite flask use sqlalchemy

--- app.py
+++ app.py
@@ -2,9 +2,38 @@ from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired  # 檢查資料是否為空
+from flask_sqlalchemy import SQLAlchemy  # 用來操作資料庫
+from datetime import datetime  # 用來建立時間紀錄
 
 app = Flask(__name__)  # create a Flask instances
+# add database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # 設置資料庫的位置
+# secret key
 app.config['SECRET_KEY'] = 'flasker'  # set a secret key for the application
+# initialize the database
+db = SQLAlchemy(app)  # create a database instance
+
+with app.app_context():
+    db.create_all()
+
+
+# create a model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # create a string
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+
+# create a user form class
+class UserForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 # create a form class
@@ -37,18 +66,24 @@ def user(name):
     return render_template('user.html', user_name=name)
 
 
-# create custom error page
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    form = UserForm()
 
-# invalid URL
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash('User Added Successfully!')
 
-
-# invalid internal server error page
-@app.errorhandler(500)
-def page_not_found(e):
-    return render_template('500.html'), 500
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template('add_user.html', form=form, name=name, our_users=our_users)
 
 
 # create Name page
@@ -64,6 +99,20 @@ def name():
         flash('Form submitted successfully!')
 
     return render_template('name.html', form=form, name=name)
+
+
+# create custom error page
+
+# invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+# invalid internal server error page
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
